f5dc510026cba82cfb3813ea73e75373463d4b81
VishnuTeja Gundam <vishnuteja.g@gytworkz.com>
2025-12-20 00:56:23 +0530

    style changes and google review link changed

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ const continueBtn = document.getElementById("continue-btn");
 
 continueBtn.addEventListener("click", () => {
   if (selectedRating >= 4) {
-    window.location.href = "https://g.page/your-restaurant/review";
+    window.location.href = "https://www.google.com/search?sca_esv=f0158aba78c19323&sxsrf=AE3TifP5Ix0WGEcRphYqlppSyW8Lsg25_A:1765561832148&si=AMgyJEtREmoPL4P1I5IDCfuA8gybfVI2d5Uj7QMwYCZHKDZ-E1EdzLhzsx_mgQL-CzX6F-XGneKXNObolDubZcQG79wUiJq64UJDw-AcMXGGLajwoUUO0zVakjJc6ziBbA2qp00Md8RD0tZ6TmTKGBvKYmEmFhY9aw%3D%3D&q=SS+CRAVE+KITCHEN+Reviews&sa=X&ved=2ahUKEwj4-NWuzriRAxWh4TgGHUZSJd0Q0bkNegQIHxAE&biw=1536&bih=730&dpr=1.25#lrd=0x3bcb9999af937fd9:0x6655ed48a446c67,3";
   } else {
     window.location.href = "https://forms.gle/your-feedback-form";
   }
